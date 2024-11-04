5022b3ddd434f9a992cac4d0795d2a7263c0b12b
EshimovShaxzod <eshimov474@gmail.com>
2024-11-04 18:40:22 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-xl font-bold">
+      Hello world
+      <Button>Click me</Button>
+    </div>
+  );
+}
